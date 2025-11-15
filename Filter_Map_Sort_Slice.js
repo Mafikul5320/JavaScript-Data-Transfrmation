c8fcc0b2cce1bfba1504251bf9451cfaf6dd2d87
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-11-15 15:48:22 +0600

    Filter Sort Slice Map

---
+++ Filter_Map_Sort_Slice.js
@@ -0,0 +1,277 @@
+const Product = [
+    {
+        "id": "p110",
+        "title": "Stainless Steel Cookware Set",
+        "description": "5-piece durable cookware set.",
+        "category": "kitchen",
+        "brand": "ChefMaster",
+        "price": 75,
+        "discount": 12,
+        "stock": 40,
+        "rating": 4.5
+    },
+    {
+        "id": "p114",
+        "title": "Women’s Casual Sneakers",
+        "description": "Comfortable sneakers for everyday wear.",
+        "category": "fashion",
+        "brand": "Adidas",
+        "price": 55,
+        "discount": 5,
+        "stock": 60,
+        "rating": 4.4
+    },
+    {
+        "id": "p108",
+        "title": "USB-C Fast Charger",
+        "description": "25W super-fast charging adapter.",
+        "category": "electronics",
+        "brand": "Samsung",
+        "price": 18,
+        "discount": 2,
+        "stock": 140,
+        "rating": 4.6
+    },
+    {
+        "id": "p106",
+        "title": "Water Bottle 1L",
+        "description": "Durable BPA-free water bottle",
+        "category": "home",
+        "brand": "AquaSafe",
+        "price": 8,
+        "discount": 0,
+        "stock": 200,
+        "rating": 4.1
+    },
+    {
+        "id": "p103",
+        "title": "Men’s Running Shoes",
+        "description": "Lightweight running shoes for daily use.",
+        "category": "fashion",
+        "brand": "Nike",
+        "price": 65,
+        "discount": 10,
+        "stock": 50,
+        "rating": 4.6
+    },
+    {
+        "id": "p122",
+        "title": "Sports Sling Bag",
+        "description": "Lightweight sling bag for sports and gym.",
+        "category": "bags",
+        "brand": "Nike",
+        "price": 25,
+        "discount": 0,
+        "stock": 60,
+        "rating": 4.2
+    },
+    {
+        "id": "p104",
+        "title": "Mechanical Gaming Keyboard",
+        "description": "RGB mechanical keyboard with blue switches.",
+        "category": "computer",
+        "brand": "Redragon",
+        "price": 40,
+        "discount": 5,
+        "stock": 70,
+        "rating": 4.7
+    },
+    {
+        "id": "p121",
+        "title": "Travel Duffel Bag",
+        "description": "Spacious travel bag for weekend trips.",
+        "category": "bags",
+        "brand": "American Tourister",
+        "price": 50,
+        "discount": 10,
+        "stock": 40,
+        "rating": 4.3
+    },
+    {
+        "id": "p109",
+        "title": "LED Desk Lamp",
+        "description": "Adjustable LED desk lamp with touch control.",
+        "category": "home",
+        "brand": "Philips",
+        "price": 22,
+        "discount": 0,
+        "stock": 90,
+        "rating": 4.3
+    },
+    {
+        "id": "p118",
+        "title": "Wooden Study Table",
+        "description": "Solid wood study table with drawer.",
+        "category": "furniture",
+        "brand": "HomeDeco",
+        "price": 90,
+        "discount": 10,
+        "stock": 15,
+        "rating": 4.3
+    },
+    {
+        "id": "p115",
+        "title": "Men’s Leather Wallet",
+        "description": "Premium leather wallet with multiple slots.",
+        "category": "fashion",
+        "brand": "Tommy Hilfiger",
+        "price": 35,
+        "discount": 0,
+        "stock": 80,
+        "rating": 4.3
+    },
+    {
+        "id": "p107",
+        "title": "Laptop Backpack",
+        "description": "Water-resistant laptop backpack with multiple pockets.",
+        "category": "bags",
+        "brand": "Lenovo",
+        "price": 32,
+        "discount": 5,
+        "stock": 55,
+        "rating": 4.4
+    },
+    {
+        "id": "p102",
+        "title": "Smartphone Tripod Stand",
+        "description": "Flexible tripod stand for mobile phones.",
+        "category": "accessories",
+        "brand": "Ulanzi",
+        "price": 12,
+        "discount": 0,
+        "stock": 80,
+        "rating": 4.3
+    },
+    {
+        "id": "p120",
+        "title": "Smart Air Purifier",
+        "description": "Compact air purifier with HEPA filter.",
+        "category": "home",
+        "brand": "Xiaomi",
+        "price": 60,
+        "discount": 8,
+        "stock": 35,
+        "rating": 4.5
+    },
+    {
+        "id": "p124",
+        "title": "Electric Kettle 1.7L",
+        "description": "Fast boiling electric kettle with auto shut-off.",
+        "category": "kitchen",
+        "brand": "Philips",
+        "price": 30,
+        "discount": 5,
+        "stock": 60,
+        "rating": 4.6
+    },
+    {
+        "id": "p111",
+        "title": "Smart LED TV 42 inch",
+        "description": "Full HD smart TV with Wi-Fi and HDMI ports.",
+        "category": "electronics",
+        "brand": "LG",
+        "price": 320,
+        "discount": 20,
+        "stock": 25,
+        "rating": 4.4
+    },
+    {
+        "id": "p116",
+        "title": "Gaming Mouse",
+        "description": "High precision wired gaming mouse with RGB.",
+        "category": "computer",
+        "brand": "Logitech",
+        "price": 30,
+        "discount": 2,
+        "stock": 90,
+        "rating": 4.6
+    },
+    {
+        "id": "p113",
+        "title": "Portable Power Bank 10000mAh",
+        "description": "High-capacity power bank with fast charging.",
+        "category": "accessories",
+        "brand": "Xiaomi",
+        "price": 22,
+        "discount": 3,
+        "stock": 100,
+        "rating": 4.5
+    },
+    {
+        "id": "p117",
+        "title": "Laptop Cooling Pad",
+        "description": "Ergonomic cooling pad for laptops up to 17 inch.",
+        "category": "computer",
+        "brand": "Cooler Master",
+        "price": 25,
+        "discount": 0,
+        "stock": 75,
+        "rating": 4.4
+    },
+    {
+        "id": "p119",
+        "title": "Bookshelf 5-tier",
+        "description": "Modern bookshelf for living room or office.",
+        "category": "furniture",
+        "brand": "Ikea",
+        "price": 75,
+        "discount": 5,
+        "stock": 25,
+        "rating": 4.4
+    },
+    {
+        "id": "p112",
+        "title": "Phone Ring Holder",
+        "description": "360-degree rotating phone ring holder.",
+        "category": "accessories",
+        "brand": "Baseus",
+        "price": 5,
+        "discount": 0,
+        "stock": 150,
+        "rating": 4.2
+    },
+    {
+        "id": "p123",
+        "title": "Non-stick Frying Pan",
+        "description": "Durable non-stick frying pan 24cm.",
+        "category": "kitchen",
+        "brand": "Tefal",
+        "price": 25,
+        "discount": 0,
+        "stock": 80,
+        "rating": 4.4
+    },
+    {
+        "id": "p105",
+        "title": "Office Chair",
+        "description": "Comfortable ergonomic office chair.",
+        "category": "furniture",
+        "brand": "ComfortPro",
+        "price": 120,
+        "discount": 15,
+        "stock": 20,
+        "rating": 4.2
+    },
+    {
+        "id": "p101",
+        "title": "Wireless Earbuds",
+        "description": "Bluetooth earbuds with noise reduction.",
+        "category": "electronics",
+        "brand": "Sony",
+        "price": 45,
+        "discount": 5,
+        "stock": 120,
+        "rating": 4.5
+    }
+];
+
+
+// Filter Category ==> Electronics.
+// Sort By ==> Rating.
+// Slice ==> first 2
+// Map ==> Transform Object shape to {name : "Name"}
+
+const TopElectionProduct = Product.filter(item => item.category === "electronics").sort((a, b) =>b.rating - a.rating).slice(0,2).map(item=>  {
+   return { name: item.title}
+})
+console.log(TopElectionProduct)
